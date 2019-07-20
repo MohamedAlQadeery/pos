049d2d6601a29b3a794e39a99f8eb7cd8a7bca37
MohamedAlqsj <moh9amad1@gmail.com>
2019-07-20 21:48:01 +0400

    first commit with localaitzion

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.dashboard.app')
+
+
+
+@section('content')
+
+    <div class="content-wrapper">
+
+        <section class="content-header">
+
+            <h1>Blank Page
+                <small>here it all starts</small>
+            </h1>
+
+            <ol class="breadcrumb">
+                <li><a href="# "><i class="fa fa-dashboard"></i> @lang('site.dashboard')</a></li>
+                <li class="active">@lang('site.users')</li>
+            </ol>
+
+        </section> <!-- end of content header section -->
+
+
+
+        <section class="content">
+    <h1>Welcome</h1>
+
+        </section><!-- end of content section -->
+
+
+    </div> <!-- content wrapper div end -->
+
+
+
+
+    @endsection
+
+
---
+++ routes/dashboard/web.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
+        Route::group(['prefix' => 'dashboard'], function () {
+
+            Route::get('/', 'DashboardController@index')->name('dashboard.index');
+
+        });
+
+    });
+
